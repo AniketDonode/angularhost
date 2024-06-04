7f1ec3be60c86d4284a7317b0968e6304e68015b
unknown <aniketdonode14@gmail.com>
2024-06-04 19:11:34 +0530

    'addedinitialCommit'

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponentComponent},
+  {path: 'about',component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
